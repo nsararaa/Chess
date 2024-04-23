5425ed6ee5757b292d31babff6ecb208a8631f42
nsararaa <149585185+nsararaa@users.noreply.github.com>
2024-04-23 09:17:22 +0500

    Initial Commit

---
+++ Chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess
+//
+//  Created by Sara Noor on 23/04/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
